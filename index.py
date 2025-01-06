88cb7922ff7af154c8a7212dbfbdcc50f80444d3
v-liuqi15 <v-liuqi15@xiaomi.com>
2025-01-06 16:47:56 +0800

    feat: 添加进度条可修改时间

--- index.py
+++ index.py
@@ -11,23 +11,24 @@ class MainWindow:
     def __init__(self,root):
         self.root = root
         self.root.title(".0_0.")
-        self.root.geometry("300x200")
+        self.root.geometry("400x200")
         self.label = tk.Label(root, text="未选择图片")
         self.label.pack(pady=20)
 
-        # 创建菜单栏
-        menubar = Menu(root)
-        # 创建退出菜单
-        menubar.add_cascade(label="退出", command=root.quit)
-        # 创建帮助菜单
-        menubar.add_cascade(label="帮助", command=self.help_info)
-        # 配置菜单栏到主窗口
-        root.config(menu=menubar)
+        menubar = Menu(root)  # 创建菜单栏
+        menubar.add_cascade(label="退出", command=root.quit)  # 创建退出菜单
+        menubar.add_cascade(label="帮助", command=self.help_info)  # 创建帮助菜单
+        root.config(menu=menubar)  # 配置菜单栏到主窗口
+
+        # 创建一个DoubleVar变量，并与Scale控件绑定
+        self.scale_var = tk.DoubleVar()
+        scale = tk.Scale(root, from_=1, to=10, orient=tk.HORIZONTAL, variable=self.scale_var)
+        scale.pack()
 
         self.button1 = tk.Button(root, text="选择图片", command=self.select_image)
-        self.button1.pack(pady=10)  # 设置水平和垂直间距
+        self.button1.pack(padx=30,side='left')  # 设置水平和垂直间距
         self.button2 = tk.Button(root, text="开始 or 重启", command=self.start)
-        self.button2.pack(pady=10) 
+        self.button2.pack(padx=30,side='right') 
 
         root.protocol("WM_DELETE_WINDOW", self.on_closing) # 绑定窗口关闭事件
 
@@ -67,19 +68,15 @@ class MainWindow:
          # 获取屏幕的逻辑宽度和高度（以像素为单位）
         logical_width = root.winfo_screenwidth()
         logical_height = root.winfo_screenheight()
-
         # 获取屏幕的物理宽度和高度（以毫米为单位）
         physical_width = root.winfo_screenmmwidth()
         physical_height = root.winfo_screenmmheight()
-
         # 将物理尺寸从毫米转换为英寸，1英寸=25.4毫米
         physical_width_inches = physical_width / 25.4
         physical_height_inches = physical_height / 25.4
-
         # 计算每英寸点数（DPI）
         dpi_x = logical_width / physical_width_inches
         dpi_y = logical_height / physical_height_inches
-
         # 假设标准DPI为96，计算缩放比
         self.scaling_ratio_x = dpi_x / 96
         self.scaling_ratio_y = dpi_y / 96
@@ -87,7 +84,7 @@ class MainWindow:
     def select_image(self):
         if self.image_path:
             self.running = False
-            messagebox.showwarning("注意", "重新选择图片需要重启进程")
+            messagebox.showwarning("注意", "必须重启进程")
         self.image_path = filedialog.askopenfilename()
         if self.image_path:
             self.label.config(text="已选择图片:" + self.image_path)
@@ -107,9 +104,9 @@ class MainWindow:
             
             # 使用模板匹配算法
             res = cv2.matchTemplate(screen_gray, template, cv2.TM_CCOEFF_NORMED)
-            threshold = 0.8  # 设置匹配阈值，根据实际情况调整
+            threshold = 0.9  # 设置匹配阈值，根据实际情况调整
             loc = np.where(res >= threshold)
-            print(f'Matched locations: {loc}',loc[0])
+            print(f'Matched locations: {loc}',loc[0],res[res >= 0.8])
             # 获取匹配区域的中心点坐标（注意：如果有多个匹配区域，这里只取第一个）
             if loc[0].size > 0:
                 self.runnum = 0  # 重置计数器
@@ -124,11 +121,11 @@ class MainWindow:
                     pyautogui.click(center_x, center_y)
                     # 可选：点击后等待一段时间，以便观察效果
                     # break  # 找到第一个匹配后就退出循环
-                time.sleep(0.1) # 休眠0.1秒
+                time.sleep(self.scale_var.get() / 10) # 休眠0.1秒
             else:
                 print('No match found')
                 self.runnum += 1  # 累加计数器
-                time.sleep(0.2) # 休眠0.2秒
+                time.sleep(self.scale_var.get() / 5) # 休眠0.2秒
                 if self.runnum > 600:  # 如果运行次数超过600次，则停止循环
                     self.running = False
                     messagebox.showinfo("提示", "停止循环")
