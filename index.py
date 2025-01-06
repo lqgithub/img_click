263d8c7c911ea157b7e46b8183cfd54b71b494ac
v-liuqi15 <v-liuqi15@xiaomi.com>
2025-01-06 10:24:51 +0800

    fix: 修改第一版

--- index.py
+++ index.py
@@ -112,12 +112,12 @@ class MainWindow:
                     pyautogui.click(center_x, center_y)
                     pyautogui.click(center_x, center_y)
                     # 可选：点击后等待一段时间，以便观察效果
-                    # break  # 找到第一个匹配后就退出循环
-                time.sleep(0.1) # 休眠0.1秒
+                    break  # 找到第一个匹配后就退出循环
+                # time.sleep(0.1) # 休眠0.1秒
             else:
                 print('No match found')
                 self.runnum += 1  # 累加计数器
-                time.sleep(0.2) # 休眠0.2秒
+                time.sleep(0.1) # 休眠0.2秒
                 if self.runnum > 600:  # 如果运行次数超过600次，则停止循环
                     self.running = False
                     messagebox.showinfo("提示", "停止循环")
